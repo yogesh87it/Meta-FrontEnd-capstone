254a4f3770196ce3223dc4c426696f122ec5e7be
yogesh87hr <yogesh87hr@gmail.com>
2025-07-02 18:45:54 +0530

    Header updated with logo and nav

---
+++ src/Components/Header.jsx
@@ -0,0 +1,49 @@
+import logo from "../assets/Logo.svg";
+export default function Header() {
+  return (
+    <header>
+      <Logo />
+      <Nav />
+    </header>
+  );
+}
+
+function Logo() {
+  return (
+    <div>
+      <img src={logo} alt="little icon logo" />
+    </div>
+  );
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+
+        <li>
+          <a href="#">About</a>
+        </li>
+
+        <li>
+          <a href="#">Menu</a>
+        </li>
+
+        <li>
+          <a href="#">Reservations</a>
+        </li>
+
+        <li>
+          <a href="#">Order Online</a>
+        </li>
+
+        <li>
+          <a href="#">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
