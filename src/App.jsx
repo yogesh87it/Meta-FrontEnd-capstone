096bb9efaaebda826f1ad368e138a9201c625fc3
yogesh87hr <yogesh87hr@gmail.com>
2025-07-05 13:09:40 +0530

    className added for grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import { useState } from "react";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import Footer from "./Components/Footer";
+
 import "./App.css";
 
 function App() {
-  return;
+  return (
+    <div className="main-container">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
